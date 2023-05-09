6febfced7740b5e820c0cd45b543ffa97391d168
Shubham-STTL <shubham.pathak@silvertouch.com>
2023-05-09 13:13:40 +0530

    [ADD] created HospitalPatient model - first commit

---
+++ hospital_management_addon/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+    #'installable' : False,
+
+
+{
+    'name': 'Hospital management TEST custom addon',
+    'version': '1.0.0',
+    'category': 'Hospital Management',
+    'sequence': -100,
+    'author' : 'Shubham Pathak',
+    'summary': 'Hospital management internal machinery Custom addon module',
+    'description': "TEST description of Hospital managements Team",
+    'depends' : [
+        #'views/menu.xml',
+        ],
+    'data' : [],
+    'demo' : [],
+    'application' : True,
+    'auto_install' : False,
+    'license': 'LGPL-3',
+    'assets' : {}
+}
+
